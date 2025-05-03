686f8d60ce068a432218febf45babea00d3fa93d
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-05-03 16:51:15 +0500

    swap method

---
+++ tasks.js
@@ -0,0 +1,13 @@
+let cup1 = 'coco';
+let cup2 = 'fanta';
+let cup3 = 'rthdrt';
+
+//qiymatni oladi  -  qiymatni beradi
+cup3 = cup1;
+cup1 = cup2
+cup2 = cup3
+cup3 =''
+
+console.log(cup1);
+console.log(cup2);
+console.log(cup3);
