a3e411ddf5d7ad41e1b193db203bbcb1745b0bed
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-05-03 16:55:21 +0500

    object <>

--- tasks.js
+++ tasks.js
@@ -1,13 +1,29 @@
-let cup1 = 'coco';
-let cup2 = 'fanta';
-let cup3 = 'rthdrt';
+// let cup1 = 'coco';
+// let cup2 = 'fanta';
+// let cup3 = 'rthdrt';
 
-//qiymatni oladi  -  qiymatni beradi
-cup3 = cup1;
-cup1 = cup2
-cup2 = cup3
-cup3 =''
+// //qiymatni oladi  -  qiymatni beradi
+// cup3 = cup1;
+// cup1 = cup2
+// cup2 = cup3
+// cup3 =''
 
-console.log(cup1);
-console.log(cup2);
-console.log(cup3);
+// console.log(cup1);
+// console.log(cup2);
+// console.log(cup3);
+
+
+var object = {
+    a: 2,
+    b: 3
+}
+
+if (object.a > object.b) {
+    console.log('A Bdan katta');
+
+} else if (object.a < object.b) {
+    console.log('B Adan katta');
+
+} else {
+    console.log('Teng');
+}
