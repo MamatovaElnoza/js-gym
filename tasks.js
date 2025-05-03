5e9ce024715afa88bf2240ed71db63675398e829
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-05-03 17:12:29 +0500

    checking odds and evens

--- tasks.js
+++ tasks.js
@@ -1,29 +1,37 @@
-// let cup1 = 'coco';
-// let cup2 = 'fanta';
-// let cup3 = 'rthdrt';
+// // let cup1 = 'coco';
+// // let cup2 = 'fanta';
+// // let cup3 = 'rthdrt';
 
-// //qiymatni oladi  -  qiymatni beradi
-// cup3 = cup1;
-// cup1 = cup2
-// cup2 = cup3
-// cup3 =''
+// // //qiymatni oladi  -  qiymatni beradi
+// // cup3 = cup1;
+// // cup1 = cup2
+// // cup2 = cup3
+// // cup3 =''
 
-// console.log(cup1);
-// console.log(cup2);
-// console.log(cup3);
+// // console.log(cup1);
+// // console.log(cup2);
+// // console.log(cup3);
 
 
-var object = {
-    a: 2,
-    b: 3
-}
+// var object = {
+//     a: 2,
+//     b: 3
+// }
 
-if (object.a > object.b) {
-    console.log('A Bdan katta');
+// if (object.a > object.b) {
+//     console.log('A Bdan katta');
 
-} else if (object.a < object.b) {
-    console.log('B Adan katta');
+// } else if (object.a < object.b) {
+//     console.log('B Adan katta');
 
-} else {
-    console.log('Teng');
+// } else {
+//     console.log('Teng');
+// }
+
+for (let index = 0; index <= 100; index++) {
+    if (index % 2 == 0) {
+        console.log(index, 'juft');
+    } else {
+        console.log(index, 'toq');
+    }
 }
